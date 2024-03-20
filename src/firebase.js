6ea881c745739f3a4878ad01dcf350db31cdf3cf
im-nirmal <arnirmal19@gmail.com>
2024-03-20 15:40:54 +0530

    firebase updated

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,25 @@
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+
+
+
+
+
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCpITnmBPiTs-xryfRrKH9IN__7QK2iWec",
-  authDomain: "documentapp-878bd.firebaseapp.com",
-  projectId: "documentapp-878bd",
-  storageBucket: "documentapp-878bd.appspot.com",
-  messagingSenderId: "664226631611",
-  appId: "1:664226631611:web:79e48320be710c5b65fe9e",
-  measurementId: "G-MH8ENX5D4D"
+  apiKey: "AIzaSyDceA8ys8pTF8PWs7lPCvaPzmm9GKK9PrM",
+  authDomain: "docapp-4bd4a.firebaseapp.com",
+  projectId: "docapp-4bd4a",
+  storageBucket: "docapp-4bd4a.appspot.com",
+  messagingSenderId: "756905000618",
+  appId: "1:756905000618:web:0797fe536291fc9a8f5ad3"
 };
 
-// const app = initializeApp(firebaseConfig);
-// export const analytics = getAnalytics(app);
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const datas = getFirestore(app)
